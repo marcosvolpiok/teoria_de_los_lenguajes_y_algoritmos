15525a0fcc01de477f6bd4b69cb6d70e3863baa6
Marcos Volpi <mark.landia@gmail.com>
2023-04-01 12:48:20 -0300

    Add exercise 1A5: Hacer un programa que solicite al usuario un número y que escriba si es par o impar.

---
+++ 1A5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+ * Hacer un programa que solicite al usuario un número y que escriba si
+ * es par o impar.
+ * */
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número");
+            int numero = int.Parse(Console.ReadLine());
+            if(numero % 2 == 0){
+                Console.WriteLine("Es par");
+            }else{
+                Console.WriteLine("Es impar");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
