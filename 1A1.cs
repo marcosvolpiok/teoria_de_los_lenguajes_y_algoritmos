9667d047236903d5722fa6cd157784849983df46
Marcos Volpi <mark.landia@gmail.com>
2023-04-01 11:50:48 -0300

    Add exercice 1 A 1: 1. Hacer un programa que solicite la edad del usuario y muestre si es mayor o menor de edad. Una persona es mayor de edad cuando ya tiene 18 años o más.

---
+++ 1A1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 1. Hacer un programa que solicite la edad del usuario y muestre si es mayor o menor de edad.
+ *   Una persona es mayor de edad cuando ya tiene 18 años o más.
+ * */
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Porfavor ingrese su edad");
+            string edad = Console.ReadLine();
+
+            if(int.Parse(edad) >= 18) {
+                Console.WriteLine("Felicitaciones! Usted ya es mayor edad");
+            } else {
+                Console.WriteLine("Todavía no es mayor edad");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
