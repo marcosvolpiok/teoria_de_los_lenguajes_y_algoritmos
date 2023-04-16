c1390d944e2521da7bc955156af0e930a61a03a0
Marcos Volpi <mark.landia@gmail.com>
2023-04-16 16:01:45 -0300

    Feature (Exercise 1-B-1)

---
+++ 2-repeticion/1B1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Hacer un programa que solicite al usuario el ingreso de un número
+ * positivo, no deberá finalizar hasta no tener un ingreso válido.
+ * */
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numeroIngresado;
+            int numeroParseado;
+            do
+            {
+                Console.WriteLine("Ingrese un número positivo");
+                numeroIngresado = Console.ReadLine();
+            } while (!int.TryParse(numeroIngresado, out numeroParseado) || numeroParseado < 0);
+        }
+    }
+}
