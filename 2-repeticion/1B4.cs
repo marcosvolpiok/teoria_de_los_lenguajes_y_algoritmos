16eb58db443f9256be00a8b14cd67f009619b2f6
Marcos Volpi <mark.landia@gmail.com>
2023-04-16 16:14:22 -0300

    Feature (Exercise 1-B-4)

---
+++ 2-repeticion/1B4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Hacer un programa que escriba la suma de los 100 primeros
+ * números.*/
+namespace ConsoleApplication13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int acumulador = 0;
+
+            for (int i = 0; i <= 100; i++ )
+            {
+                acumulador = acumulador + i;
+            }
+
+            Console.WriteLine("La suma de los 100 primeros números es: " + acumulador);
+            Console.ReadLine();
+        }
+    }
+}
