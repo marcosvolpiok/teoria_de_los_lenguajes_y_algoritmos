38b733b342b2abedd42eb1a6a42558c8130b2e4c
Marcos Volpi <mark.landia@gmail.com>
2023-04-16 16:20:47 -0300

    Featue (Exercise 1-B-5)

---
+++ 2-repeticion/1B5.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication14
+{
+    /*
+     * Hacer un programa que escriba todos los números naturales que hay
+     * desde la unidad (1) hasta un número ingresado por el usuario.
+     * */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroParseado;
+            string numeroIngresado;
+
+            do {
+                Console.WriteLine("Ingrese un número natural");
+                numeroIngresado = Console.ReadLine();
+            } while (!int.TryParse(numeroIngresado, out numeroParseado) || numeroParseado < 1);
+             
+            for(int i = 1; i <= numeroParseado; i++) {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
