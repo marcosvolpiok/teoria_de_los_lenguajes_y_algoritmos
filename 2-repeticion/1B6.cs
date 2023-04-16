1829f2f5800115df14c96f8eb2bb2d233a84cd92
Marcos Volpi <mark.landia@gmail.com>
2023-04-16 16:28:10 -0300

    Feature (Exercise 6)

---
+++ 2-repeticion/1B6.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Hacer un programa que permita introducir un número por el usuario y,
+ * escriba y cuente los múltiplos de 3 desde la unidad (1) hasta el
+ * número ingresado
+ * */
+namespace ConsoleApplication15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroParseado;
+            string numeroIngresado;
+            int acumulador = 0;
+
+            do
+            {
+                Console.WriteLine("Ingrese un número natural");
+                numeroIngresado = Console.ReadLine();
+            } while (!int.TryParse(numeroIngresado, out numeroParseado) || numeroParseado < 1);
+
+            for (int i = 1; i <= numeroParseado; i++)
+            {
+                Console.WriteLine(i);
+                if (i % 3 == 0) {
+                    Console.WriteLine("El " + i + " es múltiplo");
+                    acumulador++;
+                }
+            }
+
+            Console.WriteLine("Cantidad total de múltiplos de 3: " + acumulador);
+            Console.ReadLine();
+        }
+    }
+}
