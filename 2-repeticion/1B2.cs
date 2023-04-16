2869f2c6bb34e7e3258691417f0eb873c96ed436
Marcos Volpi <mark.landia@gmail.com>
2023-04-16 16:05:46 -0300

    Feature (Exercise 1-B-2)

---
+++ 2-repeticion/1B2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Hacer un programa que solo nos permita introducir los valores “S” o
+ * “N” en una variable, no deberá finalizar hasta no tener un ingreso
+ * válido.
+ * */
+
+namespace ConsoleApplication11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcionIngresada;
+
+            do {
+                Console.WriteLine("Ingrese \"S\" o \"N\"");
+                opcionIngresada = Console.ReadLine();
+            } while (opcionIngresada != "S" && opcionIngresada != "N");
+        }
+    }
+}
