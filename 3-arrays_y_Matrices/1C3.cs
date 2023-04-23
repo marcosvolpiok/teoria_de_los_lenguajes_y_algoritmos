99eeafd77587758c937c11af78af838a42bb17fb
Marcos Volpi <mark.landia@gmail.com>
2023-04-23 18:26:21 -0300

    Feature (exercise 1C3) Add exercise

---
+++ 3-arrays_y_Matrices/1C3.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Hacer un programa que pida el tamaño de un array, y que luego lo
+ * complete pero solo permita ingresar números múltiplos de 4.
+ * */
+namespace ConsoleApplication22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroParseado;
+            string numeroIngresado;
+            bool resultadoValidacionNumero;
+            int[] arrayDefinidoPorElUsuario;
+            int numeroParseadoArray;
+
+            do
+            {
+                Console.WriteLine("Ingrese un número positivo");
+                numeroIngresado = Console.ReadLine();
+                resultadoValidacionNumero = int.TryParse(numeroIngresado, out numeroParseado);
+            } while (!resultadoValidacionNumero || numeroParseado <= 0);
+            arrayDefinidoPorElUsuario = new int[numeroParseado];
+
+            resultadoValidacionNumero = false;
+            numeroParseadoArray = numeroParseado;
+            numeroParseado = 0;
+
+            for (int i = 0; i < numeroParseadoArray; i++)
+            {
+                do
+                {
+                    Console.WriteLine("Ingrese un número divisible por 4");
+                    numeroIngresado = Console.ReadLine();
+                    resultadoValidacionNumero = int.TryParse(numeroIngresado, out numeroParseado);
+                } while (!resultadoValidacionNumero || numeroParseado % 4 != 0);
+
+                arrayDefinidoPorElUsuario[i] = numeroParseado;
+                Console.WriteLine("Posición " + i + " = " + arrayDefinidoPorElUsuario[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
